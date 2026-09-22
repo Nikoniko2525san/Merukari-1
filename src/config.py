51c3fe7aca1270a1d6d36631c65fd1bd11224c76
Nikoniko2525san <nikoniko.nikosanki@icloud.com>
2026-09-23 01:36:45 +0900

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,15 @@
+# 利益率の最低ライン
+MIN_PROFIT_RATE = 0.30
+
+# 大型商品を除外する
+EXCLUDE_LARGE_ITEMS = True
+
+# 発送サイズの上限
+# メルカリ便などで扱いやすい範囲を想定
+MAX_SHIPPING_SIZE = 100
+
+# 送料を利益計算に含める
+INCLUDE_SHIPPING_FEE = True
+
+# メルカリ販売手数料
+MERCARI_FEE_RATE = 0.10
