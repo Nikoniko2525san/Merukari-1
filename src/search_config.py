080e38e16ec163de8b91138000a1f1427d653af8
Nikoniko2525san <nikoniko.nikosanki@icloud.com>
2026-09-23 01:53:55 +0900

    Create search_config.py

---
+++ src/search_config.py
@@ -0,0 +1,15 @@
+# 監視する検索キーワード
+SEARCH_KEYWORDS = [
+    "iPhone",
+    "iPad",
+    "MacBook",
+    "ゲーミングPC",
+    "ノートパソコン",
+    "グラフィックボード",
+    "CPU",
+    "メモリ",
+    "SSD",
+]
+
+# 1回の検索で処理する最大件数
+MAX_ITEMS_PER_KEYWORD = 100
